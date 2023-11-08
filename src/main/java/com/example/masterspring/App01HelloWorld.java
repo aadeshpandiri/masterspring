6c5e9f887469e1ff668ee1b621e9f2251c9b3315
aadesh <aadeshpandiri@gmail.com>
2023-11-09 01:43:18 +0530

    Use of Primary and Qualifier with Beans

--- src/main/java/com/example/masterspring/App01HelloWorld.java
+++ src/main/java/com/example/masterspring/App01HelloWorld.java
@@ -11,11 +11,18 @@ public class App01HelloWorld {
         System.out.println("Name : "+context.getBean("name"));
         System.out.println("Age : "+context.getBean("age"));
         System.out.println("Person : "+context.getBean("person"));
-        System.out.println("Address : "+context.getBean("address"));
+        //System.out.println("Address : "+context.getBean("address"));
         System.out.println(context.getBean("person2MethodCall"));
         System.out.println(context.getBean("person3Parameters"));
         System.out.println(context.getBean("address2"));
         //will cause error as more than one Address Bean
         //System.out.println(context.getBean(Address.class));
+
+        System.out.println(context.getBean(Person.class));
+        System.out.println(context.getBean(Address.class));
+        System.out.println(context.getBean("person5Qualifier"));
+        //print all beans , it will give array of beans so will iterate and print each bean
+        //		Arrays.stream(context.getBeanDefinitionNames())
+        //			.forEach(System.out::println);
     }
 }
--- src/main/java/com/example/masterspring/HelloWorldConfiguration.java
+++ src/main/java/com/example/masterspring/HelloWorldConfiguration.java
@@ -1,7 +1,9 @@
 package com.example.masterspring;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 record Person(String name, int age, Address address){}
 
@@ -35,12 +37,29 @@ public class HelloWorldConfiguration {
         return new Person(name,age,address3);
     }
 
+    @Bean
+    @Primary
+    //Primary Annotation will declare bean as default one ,
+    // so while conflict occurs this bean apply by default
+    public Person person4Parameters(String name, int age, Address address) {
+        //name,age,address2
+        return new Person(name, age, address); //name, age
+    }
+
+    @Bean
+    public Person person5Qualifier(String name, int age, @Qualifier("address3qualifier") Address address) {
+        //name,age,address2
+        return new Person(name, age, address); //name, age
+    }
+
     @Bean(name = "address2")
+    @Primary
     public Address address() {
         return new Address("Theatre Road","Hyd");
     }
 
     @Bean(name = "address3")
+    @Qualifier("address3qualifier")
     public Address address3() {
         return new Address("Sai Ram Towers", "HYD");
     }
