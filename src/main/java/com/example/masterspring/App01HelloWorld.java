24fb5e1234ce9981a44b585eaec5c4cf3da7ba74
aadesh <aadeshpandiri@gmail.com>
2023-11-09 01:28:07 +0530

    Created Multiple Beans

--- src/main/java/com/example/masterspring/App01HelloWorld.java
+++ src/main/java/com/example/masterspring/App01HelloWorld.java
@@ -9,5 +9,8 @@ public class App01HelloWorld {
 
         var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
         System.out.println("Name : "+context.getBean("name"));
+        System.out.println("Age : "+context.getBean("age"));
+        System.out.println("Person : "+context.getBean("person"));
+        System.out.println("Address : "+context.getBean("address"));
     }
 }
--- src/main/java/com/example/masterspring/HelloWorldConfiguration.java
+++ src/main/java/com/example/masterspring/HelloWorldConfiguration.java
@@ -3,11 +3,30 @@ package com.example.masterspring;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person(String name, int age){}
+
+record Address(String firstLine, String city){ };
+
 @Configuration
 public class HelloWorldConfiguration {
 
     @Bean
     public String name(){
         return "Aadesh";
+    }
+
+    @Bean
+    public int age(){
+        return 23;
+    }
+
+    @Bean
+    public Person person(){
+        return new Person("Bubu",22);
+    }
+
+    @Bean
+    public Address address(){
+        return new Address("Theatre Road","Hyderabad");
     }
 }
