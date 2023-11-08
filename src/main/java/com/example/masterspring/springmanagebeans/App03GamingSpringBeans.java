671ad324ff7b20cc2637a77055fc00a396176150
aadesh <aadeshpandiri@gmail.com>
2023-11-09 01:59:25 +0530

    Wrap Context under try catch

--- src/main/java/com/example/masterspring/springmanagebeans/App03GamingSpringBeans.java
+++ src/main/java/com/example/masterspring/springmanagebeans/App03GamingSpringBeans.java
@@ -6,13 +6,13 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App03GamingSpringBeans {
     public static void main(String[] args){
-        var context = new AnnotationConfigApplicationContext(GamingConfiguration.class);
+        //wrap it under try catch block , will use new try here
+        try(var context = new AnnotationConfigApplicationContext(GamingConfiguration.class)){
+            //Gaming Console Bean has Mario Game Instance then get that and call up method
+            context.getBean(GamingConsole.class).up();
 
-        //Gaming Console Bean has Mario Game Instance then get that and call up method
-        context.getBean(GamingConsole.class).up();
-
-        //GameRunner Bean will take game instance and call run method
-        context.getBean(GameRunner.class).run();
-
+            //GameRunner Bean will take game instance and call run method
+            context.getBean(GameRunner.class).run();
+        }
     }
 }
