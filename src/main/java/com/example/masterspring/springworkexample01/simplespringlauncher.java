8974c1709c38584452890bf772c2c81f1a6f8420
aadesh <aadeshpandiri@gmail.com>
2023-11-10 01:26:40 +0530

    fields Autowiring

--- src/main/java/com/example/masterspring/springworkexample01/simplespringlauncher.java
+++ src/main/java/com/example/masterspring/springworkexample01/simplespringlauncher.java
@@ -1,5 +1,6 @@
 package com.example.masterspring.springworkexample01;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +10,9 @@ import java.util.Arrays;
 
 @Component
 class BusinessClass{
+    @Autowired
     Dependency1 dependency1;
+    @Autowired
     Dependency2 dependency2;
     public String toString() {
         return "Using " + dependency1 + " and " + dependency2;
