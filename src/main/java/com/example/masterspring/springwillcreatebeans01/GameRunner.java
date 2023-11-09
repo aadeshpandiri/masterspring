569a28897893bb5b41c023204049d429e4786eb8
aadesh <aadeshpandiri@gmail.com>
2023-11-09 22:43:37 +0530

    Qualifier Annotation for Spring Beans

--- src/main/java/com/example/masterspring/springwillcreatebeans01/GameRunner.java
+++ src/main/java/com/example/masterspring/springwillcreatebeans01/GameRunner.java
@@ -1,18 +1,19 @@
 package com.example.masterspring.springwillcreatebeans01;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GameRunner {
 
     @Autowired
+    @Qualifier("MarioGameQualifier")
     GamingConsole game;
-    public GameRunner(){}
 
-    public GameRunner(GamingConsole game){
-        this.game=game;
-    }
+//    public GameRunner(@Qualifier("MarioGameQualifier") GamingConsole game){
+//        this.game=game;
+//    }
     public void run(){
         System.out.println("Running game: " + game);
         game.up();
--- src/main/java/com/example/masterspring/springwillcreatebeans01/MarioGame.java
+++ src/main/java/com/example/masterspring/springwillcreatebeans01/MarioGame.java
@@ -1,8 +1,10 @@
 package com.example.masterspring.springwillcreatebeans01;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@Qualifier("MarioGameQualifier")
 public class MarioGame implements GamingConsole {
     public void up(){ System.out.println("MARIO UP"); }
     public void right(){ System.out.println("MARIO RIGHT"); }
